6945c4e5386524cbbca93382af004a183feebe4f
nrglen <140910047+nrglen@users.noreply.github.com>
2024-08-12 19:33:21 +0000

    adding csv database, libraries requiered file and correction for the main app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ import plotly.express as px
 #st.markdown("## My first streamlit application dashboard!")
 
 DATA_URL = (
-"/home/rhyme/Desktop/Project/Motor_Vehicle_Collisions_-_Crashes.csv"
+"Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 
 st.title("Motor vehicle collisions New York City")
 st.markdown("This application is a streamlit dashboard that can be used "
-"to analize motor vehicle collision in NYC Ã°ÂÂÂ¥Ã°ÂÂÂ")
-@st.cache(persist=True)
+"to analize motor vehicle collision in NYC ")
+@st.cache_data(persist=True)
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[["CRASH_DATE", "CRASH_TIME"]])
     data.dropna(subset=["LATITUDE", "LONGITUDE"], inplace=True)
@@ -47,3 +47,59 @@ st.write(pdk.Deck(
         "latitude":midpoint[0],
         "longitude": midpoint[1],
         "zoom":11,
+        "pitch": 40 ,
+
+    },
+    layers=[
+        pdk.Layer(
+        "HexagonLayer",
+        data=data[["date/time", "latitude", "longitude"]],
+        get_position=["longitude", "latitude"],
+
+
+        opacity=0.8,
+        stroked=False,
+
+        radius = 300,
+        extruded = True,
+        pickable = True,
+        elevation_scale = 6,
+        elevation_range= [0, 1000],
+
+        ),
+    ],
+))
+
+
+st.subheader("Breakdown by minute between %i:00 and %i:00" % (hour,( hour + 1)%24))
+
+filtered = data[
+    (data["date/time"].dt.hour >= hour) & (data["date/time"].dt.hour < (hour + 1))
+]
+
+hist = np.histogram(filtered["date/time"].dt.minute, bins=60, range=(0, 60))[0]
+
+chart_data = pd.DataFrame({"minute":range(60), "crashes":hist})
+fig = px.bar(chart_data, x="minute", y="crashes", hover_data=["minute", "crashes"], height=400)
+st.write(fig)
+
+
+
+st.write("Top 5 dangerous streets by affected type")
+select = st.selectbox("Affected type of people", ["Pedestrians", "Cyclists", "Motorists"])
+
+
+if select == "Pedestrians":
+    st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(by=["injured_pedestrians"], ascending=False).dropna(how="any")[:5])
+
+elif select == "Cyclists":
+    st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].sort_values(by=["injured_cyclists"], ascending=False).dropna(how="any")[:5])
+
+else:
+    st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(by=["injured_motorists"], ascending=False).dropna(how="any")[:5])
+
+
+
+if st.checkbox("Show Raw Data",False):
+    st.subheader("Raw Data")
+    st.write(data)
